3188cf716da773af22811f7a9db06eee443a595d
ari-gc <100432830+ari-gc@users.noreply.github.com>
2024-02-07 23:05:06 -0500

    Loading data from JSON file

--- server.js
+++ server.js
@@ -1,32 +1,18 @@
+
 const express = require('express');
 const app = express();
 const port = 3000;
 
 app.use('/', express.static('public'));
 
-const budget = {
-    myBudget: [
-        {
-            title: 'Boba',
-            budget: 25
-        },
-        {
-            title: 'Rent',
-            budget: 600
-        },
-        {
-            title: 'Grocery',
-            budget: 150
-        },
-    ]
-};
+const budget = require("./c01.json");
 
 app.get('/hello', (req, res) => {
     res.send("Hello");
 });
 
 app.get('/budget', (req, res) => {
-    res.json(budget);
+    res.json(budget)
 });
 
 app.listen(port, () => {
